ee0161f1082ba0dc10da0a55da9267d2c9dff315
veysel <vinanc72@gmail.com>
2017-04-06 00:24:13 +0300

    General bu fixed. Logger added. Counter added

--- src/app/Components/test.component.ts
+++ src/app/Components/test.component.ts
@@ -10,17 +10,32 @@ import { TestService } from '../Services/test.service';
 export class TestComponent implements OnInit {
     public apiUrl = "";
     public result: any;
+    public log: any;
+    public count: any;
 
     constructor(
         private _testService: TestService
     ) {
+        this.getCount();
     }
 
     ngOnInit() { }
 
     public getInfo() {
-        this._testService.getInfo(this.apiUrl).subscribe(x => {
-            this.result = JSON.stringify(x);
-        });
+        this._testService.getInfo(this.apiUrl).subscribe(
+            data => {
+                this.log = "Başarılı";
+                this.result = JSON.stringify(data);
+                this._testService.updateCount(parseInt(this.count) + 1).subscribe(x => { this.getCount(); });
+            },
+            error => {
+                this.result = "";
+                this.log = error;
+            }
+        );
+    }
+
+    public getCount() {
+        this._testService.getCount().subscribe(data => { this.count = data.sayac; });
     }
 }
--- src/app/Services/test.service.ts
+++ src/app/Services/test.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Http } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs';
 
 import 'rxjs/add/operator/map';
@@ -8,20 +8,28 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class TestService {
+    private countUrl = "https://api.myjson.com/bins/12mz97";
+    private headers: Headers;
 
     constructor(
         private _http: Http
     ) {
+        this.headers = new Headers();
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Access-Control-Allow-Origin', '*');
 
     }
 
     public getInfo(apiUrl: string) {
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Access-Control-Allow-Origin', '*');
-
-        return this._http.get(apiUrl, headers).map(x => x.json());
+        return this._http.get(apiUrl, this.headers).map(x => x.json());
     }
 
+    public getCount() {
+        return this._http.get(this.countUrl, this.headers).map(x => x.json());
+    }
+
+    public updateCount(count) {
+        return this._http.put(this.countUrl, { sayac: count }).map(x => x.json());
+    }
 
 }
